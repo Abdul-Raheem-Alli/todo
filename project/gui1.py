39598a2fea38ff3ad36ff23103698c9c1d246749
edit <alliayomide90@gmail.com>
2023-09-13 09:24:39 -0700

    16

--- project/gui1.py
+++ project/gui1.py
@@ -1,1 +1,10 @@
 from project.text import get_todos, write_todos
+import PySimpleGUI as sg
+
+label = sg.Text("Type in a to do ")
+input_box = sg.InputText(tooltip="enter a todo")
+add_button = sg.Button("Add")
+
+window = sg.Window('my todo app', layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
